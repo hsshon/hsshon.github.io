7953cc73eb3e322de20a3705ad553ccf13de2354
hsshon <hs.shon91@gmail.com>
2025-09-04 19:15:16 +0000

    Deploying to gh-pages from @ hsshon/hsshon.github.io@63454710576ab5caad72be9140e18fdc0987dc2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,9 +434,9 @@ ninja.data = [{
           description: "Funded by National Research Foundation of Korea",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-urban-mobility-network-analysis-amp-forecasting-with-attention-mechanisms-and-graph-neural-networks",
+          title: 'Urban Mobility Network Analysis &amp;amp; Forecasting with Attention Mechanisms and Graph Neural Networks...',
+          description: "Funded by National Research Foundation of Korea",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
