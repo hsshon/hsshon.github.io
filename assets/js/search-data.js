5f9b2dc7c727fb309c54bc5389f0d7e3f33f5d7f
hsshon <hs.shon91@gmail.com>
2025-09-04 02:35:01 +0000

    Deploying to gh-pages from @ hsshon/hsshon.github.io@956c06f4c93c783d4927b7e6a5c76aaef189214d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           section: "News",},{id: "news-relocated-to-the-united-states",
           title: 'Relocated to the United States',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-taxi-operation-information-management-system",
+          title: 'Taxi Operation Information Management System',
+          description: "Analyzed large-scale digital tachograph (DTG) taxi data to identify demand hotspots and prototyped detectors for regulatory violations/unauthorized operations.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
